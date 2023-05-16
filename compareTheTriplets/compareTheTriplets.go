532d1a166e1f8f58b9b50b7a58e39db0c1b751f7
jachym <jachym.jahoda@icloud.com>
2023-05-16 09:31:58 +0200

    Finished compare the triplets

--- compareTheTriplets/compareTheTriplets.go
+++ compareTheTriplets/compareTheTriplets.go
@@ -3,11 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var a, b [3]int
-	fmt.Scan(&a[0], &a[1], &a[2])
-	fmt.Scan(&b[0], &b[1], &b[2])
 	var alice, bob int
-	for i := 0; i < 3; i++ {
+	var a, b [3]int
+	fmt.Scan(&a[0], &a[1], &a[2], &b[0], &b[1], &b[2])
+	for i := range a {
 		if a[i] > b[i] {
 			alice++
 		} else if b[i] > a[i] {
